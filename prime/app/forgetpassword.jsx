094233d3db2a5cb995c2c4b5def4420825b90da2
anuruddh-1905 <anuruddhpratap67@gmail.com>
2026-06-14 15:30:48 +0530

    fix: resolve fetch template literal syntax exception

--- prime/app/forgetpassword.jsx
+++ prime/app/forgetpassword.jsx
@@ -22,7 +22,7 @@ export default function ForgetPassword() {
         }
         
         try {
-            const res = await fetch("http://127.0.0.1:5000/api/auth/reset-password", {
+            const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/api/auth/reset-password`, {
                 method: "POST",
                 headers: { "Content-Type" : "application/json"},
                 body: JSON.stringify({
